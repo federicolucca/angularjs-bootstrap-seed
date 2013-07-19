242ef2b96cc81427373685aedb46803c7d0ce4f7
federicolucca <federico@federico-Precision-M4700>
2013-07-19 17:48:58 +0200

    carousel

--- app/js/services.js
+++ app/js/services.js
@@ -6,4 +6,14 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+  value('version', '0.1')
+.factory('DataSource', ['$http',function($http){
+       return {
+           get: function(fileName,callback){
+                $http.get(fileName).
+                success(function(data, status) {
+                    callback(data);
+                });
+           }
+       };
+    }]);
